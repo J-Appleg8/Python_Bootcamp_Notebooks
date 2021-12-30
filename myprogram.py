a2340f9185820ad2eff41a96218744c877273162
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2021-12-29 17:34:39 -0800

    finished modules and packages section

---
+++ myprogram.py
@@ -0,0 +1,6 @@
+from p9_MyMainPackage import some_main_script
+from p9_MyMainPackage.SubPackage import mysubscript
+
+some_main_script.report_main()
+
+mysubscript.sub_report()
---
+++ p9_MyMainPackage/some_main_script.py
@@ -0,0 +1,2 @@
+def report_main():
+    print("Hey im a function inside some_main_script in main package")
